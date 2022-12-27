2c260c246d93e96c1d4223925ecba3c1e3154afb
Dustin Ray <dustinvonsandwich@gmail.com>
2022-12-27 04:02:58 -0800

    E521 gen point and ID point implemented

--- e521/src/main.rs
+++ e521/src/main.rs
@@ -1,7 +1,11 @@
-use std::{str::FromStr, ops::{Sub}};
-use num_bigint::{BigInt};
-
-struct e521 {
+use std::{str::FromStr, ops::{Sub, Mul}};
+use num_bigint::{BigInt, Sign};
+use num::Integer;
+use num::One;
+use num::Zero;
+// use crypto_bigint::{};
+#[derive(Debug)]
+struct E521 {
     x: BigInt,
     y: BigInt,
     p: BigInt,
@@ -11,11 +15,13 @@ struct e521 {
 }
 
 fn main() {
-    println!("value of r: {:?}", get_r());
-    println!("value of p: {:?}", get_p());
-    println!("value of n: {:?}", get_n());
-    println!("value of d: {:?}", get_d());
+    // println!("value of r: {:?}", get_r());
+    // println!("value of p: {:?}", get_p());
+    // println!("value of n: {:?}", get_n());
+    // println!("value of d: {:?}", get_d());
 
+    println!("gen point y coord: {:?}", get_e521_gen_point(false).y);
+    // println!("id point: {:?}", get_e521_id_point());
 }
 
 fn get_r() -> BigInt {
@@ -34,15 +40,104 @@ fn get_p() -> BigInt {
 }
 
 fn get_n() -> BigInt {
-
     let n = get_r();
     let n = n.checked_mul(&BigInt::from(4)).unwrap();
     return n;
 }
 
-fn get_d() -> BigInt {
+fn get_d() -> BigInt { return Some(BigInt::from(-376014)).unwrap();}
 
-    let d = BigInt::from(-376014);
-    return d;
 
+
+// Generates the neutral point (0, 1)
+fn get_e521_id_point() -> E521 {
+    let point = E521{
+        x: BigInt::from(0),
+        y: BigInt::from(1),
+        p: get_p(),
+        d: get_d(),
+        r: get_r(), 
+        n: get_n()
+    };
+    point
+}
+
+// Gets point for arbitrary (x, y) TODO verify point is on curve
+fn get_e521_point(x: BigInt, y: BigInt) -> E521 {
+    let point = E521{
+        x: x,
+        y: y,
+        p: get_p(),
+        d: get_d(),
+        r: get_r(), 
+        n: get_n()
+    };
+    point
+}
+
+// Gets point for arbitrary (x, y) TODO verify point is on curve
+fn get_e521_gen_point(lsb: bool) -> E521 {
+    let x = BigInt::from(4);
+    let new_x = x.clone();
+    let point = E521{
+        x: x,
+        y: solve_for_y(&new_x, get_p(), lsb),
+        p: get_p(),
+        d: get_d(),
+        r: get_r(), 
+        n: get_n()
+    };
+    point
+}
+
+fn solve_for_y(x: &BigInt, p: BigInt, msb: bool) -> BigInt {
+    let num = BigInt::from(1) - x.pow(2);
+    let num = mod_formula(&num, &p);
+    let denom = BigInt::from(376_014) * x.pow(2) + BigInt::from(1);
+    let denom = mod_formula(&denom, &p);
+    let denom = mod_inv(&denom, &p);
+    let radicand = num * denom;
+    let y = sqrt(&radicand, msb);
+    y
+}
+
+fn mod_formula(a: &BigInt, b: &BigInt) -> BigInt {
+    ((a % b) + b) % b
+}
+
+fn mod_inv(n: &BigInt, p: &BigInt) -> BigInt {
+    if p.is_one() { return BigInt::one() }
+
+    let (mut a, mut m, mut x, mut inv) = (n.clone(), p.clone(), BigInt::zero(), BigInt::one());
+    while a < BigInt::zero() { a += p }
+    while a > BigInt::one() {
+        let (div, rem) = a.div_rem(&m);
+        inv -= div * &x;
+        a = rem;
+        std::mem::swap(&mut a, &mut m);
+        std::mem::swap(&mut x, &mut inv);
+    }
+ 
+    if inv < BigInt::zero() { inv += p }
+    inv
+}
+
+fn sqrt(v: &BigInt, lsb: bool) -> BigInt {
+    if v.sign() ==  Sign::NoSign{
+        return BigInt::from(0);
+    }
+    let p = get_p();
+    let r = v.modpow(&((p.clone() >> 2) + 1), &p);
+    if !r.bit(0).eq(&lsb) {
+        let new_r = &p - r; // correct the lsb
+        let newr2 = new_r.clone();
+        let return_r = new_r.clone();
+        let bi = mod_formula(&new_r.mul(newr2).sub(v), &p);
+        if bi.sign() == Sign::NoSign {
+            return return_r;
+        } else {
+            return BigInt::from(0);
+        }
+    }
+    r
 }
